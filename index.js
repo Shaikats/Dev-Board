1f1da0d52f9158cfb33803e44e65bacca1b9e7da
Shaikat <shaikat700@gmail.com>
2025-03-02 02:40:24 +0600

    Header

---
+++ index.js
@@ -0,0 +1,9 @@
+let totalCompletedTaskCount = parseInt(document.getElementById('total-completed-task-count').innerText);
+let assignedTaskCount = parseInt(document.getElementById('assigned-task-count').innerText);
+
+completeTask('c1', 'item-1');
+completeTask('c2', 'item-2');
+completeTask('c3', 'item-3');
+completeTask('c4', 'item-4');
+completeTask('c5', 'item-5');
+completeTask('c6', 'item-6');
